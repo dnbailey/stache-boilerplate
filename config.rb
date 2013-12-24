24ce1a5f3b4c16c2c1aa753ab62a4d33210330cd
Nathan Bailey <dev@dnbailey.org>
2013-12-24 01:27:49 -0500

    Fix stylesheet and sass config to allow Wordpress to recognize theme

--- config.rb
+++ config.rb
@@ -4,9 +4,6 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "scripts"
 
-# You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed
-
-# To enable relative paths to assets via compass helper functions.
-# note: this is important in wordpress themes for sprites
+line_comments = false  
 relative_assets = true
