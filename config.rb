637531b5c45f9288cd724b48b15a49065e77052f
Nathan Bailey <dev@dnbailey.org>
2013-12-24 00:45:29 -0500

    Add compass and sass support

---
+++ config.rb
@@ -0,0 +1,12 @@
+http_path = "/"
+css_dir = "."
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "scripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions.
+# note: this is important in wordpress themes for sprites
+relative_assets = true
