2d2e7b87002ac261588c6aa6d2fce8fbb37df599
Nathan Bailey <dev@dnbailey.org>
2014-01-04 13:40:48 -0500

    Add link to post title

--- content.php
+++ content.php
@@ -2,6 +2,6 @@
 	// Template partial for displaying content in single, index, archive, and search 
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('block'); ?>>
-	<h2><?php the_title(); ?></h2>
+	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	<?php the_content(); ?>
 </article>
